53903956747529a66cc59d5738a4c9ea0ae25c6a
rungrawee <rungrawee.alek@gmail.com>
2026-04-16 11:36:43 +0700

    feat:update layout

--- config.js
+++ config.js
@@ -2,3 +2,7 @@ window.APP_CONFIG = {
   PYTHON_API_BASE_URL: "https://rungrawee-reportseller.hf.space",
   PYTHON_API_URL: "https://rungrawee-reportseller.hf.space/remove-background",
 };
+//window.APP_CONFIG = {
+  //PYTHON_API_BASE_URL: "http://localhost:9000",
+  //PYTHON_API_URL: "http://localhost:9000/remove-background",
+//};
